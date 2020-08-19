8ba7b174b831659d7a6adfaebb4ccb2104bbc693
Jyles Coad-Ward <jyles@dariox.club>
2020-08-19 15:51:19 +0800

    Update webpack.config.js

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+        entry: './src/index.js',
+        module: {
+            rules: [
+                {
+                test: /\.css$/i,
+                use:
+                    [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
 };
