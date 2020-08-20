84d6c1006a48d21bc3c23e3e3a7b947fe6a7d670
Jyles Coad-Ward <jyles@dariox.club>
2020-08-20 17:57:21 +0800

    Update init.js

--- src/processing/init.js
+++ src/processing/init.js
@@ -1,20 +1,27 @@
 /*
     == File Description ==
-        + Decrept Token
+        + Decrept/Encrypt Token
         + Initialize PUBG API Connection
         + Function Handler
         + Error Handler
         + Console Output
 */
 
+/* === Init PUBG API === */
+var APIKEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjN2I4ODYwMC0zMmRkLTAxMzgtMmU3MC02MzE5NDU3ZGU0YzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTgxODUyNjgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Imp5bGVzY29hZHdhcmQtIn0.5mJeV8fZiZ_8MEdWK57l4JmlNWMaz2fIDnJVHAR7HFE";
+
 var PUBG = require("battlegrounds");
-
-
-const api = new PUBG("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjN2I4ODYwMC0zMmRkLTAxMzgtMmU3MC02MzE5NDU3ZGU0YzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTgxODUyNjgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Imp5bGVzY29hZHdhcmQtIn0.5mJeV8fZiZ_8MEdWK57l4JmlNWMaz2fIDnJVHAR7HFE")
+const api = new PUBG(APIKEY);
 async function testConnection() {
     try {
         const res = await api.getPlayers({ names: ['xSuperSeed'] });
-        console.div("[PUBG_API] Connected to PUBG Servers.");
+		res.forEach(async(m) => {
+			delete(m._api.apikey)
+		})
+		console.log("[PUBG_API] => [ConnectionTestResult]",res[0]);
+		if (res[0].id !== undefined){
+        	console.div("[PUBG_API] Connected to PUBG Servers.");
+		}
         return true;
     } catch (e) {
         console.error(e);
@@ -23,4 +30,4 @@ async function testConnection() {
     }
 }
 
-testConnection();
+var connectionWorking = testConnection();
