483718c91047e70b95fd4b98c9deeff39d0cd097
Jyles Coad-Ward <jyles@dariox.club>
2020-08-20 16:15:13 +0800

    build2
    Now checks if connection works, if not a message is sent into console

--- src/processing/init.js
+++ src/processing/init.js
@@ -6,3 +6,21 @@
         + Error Handler
         + Console Output
 */
+
+var PUBG = require("battlegrounds");
+
+
+const api = new PUBG("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjN2I4ODYwMC0zMmRkLTAxMzgtMmU3MC02MzE5NDU3ZGU0YzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTgxODUyNjgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Imp5bGVzY29hZHdhcmQtIn0.5mJeV8fZiZ_8MEdWK57l4JmlNWMaz2fIDnJVHAR7HFE")
+async function testConnection() {
+    try {
+        const res = await api.getPlayers({ names: ['xSuperSeed'] });
+        console.div("[PUBG_API] Connected to PUBG Servers.");
+        return true;
+    } catch (e) {
+        console.error(e);
+        console.div("[PUBG_API] Error Occurred, Check Console.");
+        return false;
+    }
+}
+
+testConnection();
