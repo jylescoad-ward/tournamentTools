52567b4060b8c80e0fe5248e996c75da404f7558
Jyles Coad-Ward <jyles@dariox.club>
2020-08-20 14:48:06 +0800

    Update index.js
    added dropdown.js
    
    prints that page is ready when document is ready from jquery

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import _ from 'lodash';
 import $ from 'jquery';
 
 import "./customConsole.js";
+import "./dropdown.js";
 
 console.debug(`[DocPathName] ${document.location.pathname}`);
 
-console.div("[System] Ready!");
-
-setTimeout(function() {
-    console.div("[User] Fetching UserID of 'SeedVEVO'");
-},1500);
+$(document).ready(() => {
+    console.div(`[System] Page Ready!`);
+})
