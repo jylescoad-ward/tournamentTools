3b02fc09614c8c105d490a7ebd2a963aecd0249f
Jyles Coad-Ward <jyles@dariox.club>
2020-08-20 17:57:11 +0800

    anti-debugger for prod

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import "./processing/init.js";
 
 console.debug(`[DocPathName] ${document.location.pathname}`);
 
+if (require("./../webpack.config.js").mode === "production") {
+	setTimeout(function() {
+		debugger;
+	},50);
+}
+
 $(document).ready(() => {
     console.div(`[System] Page Ready!`);
 })
