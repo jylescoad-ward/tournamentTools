901b76ab252d955ab1f746007f2c2486eafe0ddd
Jyles Coad-Ward <jyles@dariox.club>
2020-08-21 15:12:27 +0800

    added randomString

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import "./processing/init.js";
 
 console.debug(`[DocPathName] ${document.location.pathname}`);
 
+global.randomString = function(length) {
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    var retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
 global.downloadString = function(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
