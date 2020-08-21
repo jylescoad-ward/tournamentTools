c9a7702201bc23414c4f0af118b0bf4fd4d48e0d
Jyles Coad-Ward <jyles@dariox.club>
2020-08-21 16:24:28 +0800

    processMatch updated and working

--- src/processing/exportMatch.js
+++ src/processing/exportMatch.js
@@ -2,38 +2,35 @@ var $ = require("jquery");
 var md5 = require("md5");
 var moment = require("moment");
 
-module.exports = async function() {
+module.exports = function() {
 	if (!PUBG_connectionWorking) return;
 
-	$("#exportMatchRAW-submit").click(() => {
+	$("#exportMatchRAW-submit").click(async () => {
 		try {
 			console.div("[PUBG_API] [exportMatch] Checking Fields");
 			if ($("#exportMatchRAW-id").val().length !== 36) {
 				console.div("[PUBG_API] [exportMatch] Invalid MatchID");
 			} else {
-				PUBG_api.getMatch({ id: $("#exportMatchRAW-id").val() }).then((res) => {
-					console.log(res);
-					if (res.id === undefined) {
-						console.div("[PUBG_API] [exportMatch] An error might have occurred, Check Console");
-					} else {
-						console.div("[PUBG_API] [exportMatch] Match Fetched Successfully");
-						$("#exportMatchRAW-download").show();
-						$("#exportMatchRAW-view").show();
+				var result = await require("./processMatch.js")($("#exportMatchRAW-id").val());
+				if (result === true) {
+					console.div("[PUBG_API] [exportMatch] An Error Occured.");
+					return;
+				} else {
+					$("#exportMatchRAW-download").show();
+					$("#exportMatchRAW-view").show();
 
-						//let matchTime = new Date(res.attributes.createdAt).getTime())/1000;
-						var dsFN_o = {
-							"antiDupePrefix": randomString(16),
-							"matchTime": moment(res.attributes.createdAt).unix(),
-							"hash": md5("id_"+res.id),
-						};
-						//var downloadString_fileName = `match-${dsFN_o.antiDupePrefix}-${dsFN_o.matchTime}-${dsFN_o.hash}.json`
-						var downloadString_fileName = `match-${dsFN_o.antiDupePrefix}-${dsFN_o.matchTime}.json`;
-						$("#exportMatchRAW-download").click(() => {
-							downloadString(downloadString_fileName,JSON.stringify(res));
-							console.div("[PUBG_API] [exportMatch] Downloaded Match "+res.id);
-						})
-					}
-				});
+					var dsFN_o = {
+						"antiDupePrefix": randomString(16),
+						"matchTime": moment(result.attributes.createdAt).unix(),
+						"hash": md5("id-"+result.id+" t-"+moment(result.attributes.createdAt).unix()),
+					};
+					var downloadString_fileName = `match-${dsFN_o.antiDupePrefix}-${dsFN_o.matchTime}.json`;
+					console.div("[PUBG_API] [exportMatch] Match Ready to Download or View");
+					$("#exportMatchRAW-download").click(() => {
+						downloadString(downloadString_fileName,JSON.stringify(result,null,"\t"));
+						console.div("[PUBG_API] [exportMatch] Downloaded Match "+result.id);
+					})
+				}
 			}
 		} catch(e) {
 			console.div("[PUBG_API] [exportMatch] An Error Occurred, Check Console");
--- src/processing/processMatch.js
+++ src/processing/processMatch.js
@@ -1,4 +1,15 @@
-module.exports = async function() {
+module.exports = async function(matchID) {
 	if (!PUBG_connectionWorking) return;
+	console.log(matchID)
 
+	console.div("[PUBG_API] [processMatch] Checking MatchID");
+	const res = await PUBG_api.getMatch({ id: matchID })
+	if (res.id !== matchID) {
+		console.div("[PUBG_API] [processMatch] MatchID may be incorrect?");
+		console.div("[PUBG_API] [processMatch] An error might have occured. Check console for more information.");
+		return true;
+	} else {
+		console.div("[PUBG_API] [processMatch] MatchID Valid",res);
+		return res;
+	}
 }
