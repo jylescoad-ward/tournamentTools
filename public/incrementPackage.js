c8825c71b2b0d6b36767814f8052fc1824de93d9
Jyles Coad-Ward <jyles@dariox.club>
2020-08-24 17:23:45 +0800

    added timestamp to build info

--- public/incrementPackage.js
+++ public/incrementPackage.js
@@ -5,6 +5,7 @@ const file = require(fileName);
 file.build.number++;
 var curDate = new Date();
 file.build.date = `${curDate.getFullYear()}_${curDate.getMonth()}_${curDate.getDate()}`;
+file.build.timestamp = Math.round(curDate.valueOf()/1000);
 
 fs.writeFile(fileName, JSON.stringify(file,null,"\t"), function writeJSON(err) {
   if (err) return console.log(err);
