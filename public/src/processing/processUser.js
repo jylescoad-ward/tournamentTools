24c2d8c630510502fb2ecdd4abbd528b82d9fc38
Jyles Coad-Ward <jyles@dariox.club>
2020-08-26 13:54:59 +0800

    added support for viewing archived users

--- public/src/processing/processUser.js
+++ public/src/processing/processUser.js
@@ -11,3 +11,23 @@ module.exports = async function(nameGiven) {
 		console.error(e);
 	}
 }
+
+
+var $ = require("jquery");
+function handleFileSelect(evt){
+	$("#processUser-submit").click(()=>{
+		var f = document.getElementById("uploadPastUser_file").files[0]
+
+		var reader = new FileReader();
+		reader.onload = ((theFile)=>{
+			return function(e) {
+				var parsedJSON = JSON.parse(e.target.result)
+				$("#viewDataField").html(require("./UserToHTML.js").force(parsedJSON));
+				console.log("[processUser] Parsed Match",parsedJSON);
+			};
+		})(f);
+		reader.readAsText(f);
+	})
+}
+
+document.getElementById('uploadPastUser_file').addEventListener('change', handleFileSelect, false);
