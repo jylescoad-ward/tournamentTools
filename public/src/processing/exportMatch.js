ee6e1351840ebedaa5365f177fe1d51cfb75dade
Jyles Coad-Ward <jyles@dariox.club>
2020-08-26 13:54:30 +0800

    Update exportMatch.js
    main logic is now in its own function so this can support viewing matches from user archives

--- public/src/processing/exportMatch.js
+++ public/src/processing/exportMatch.js
@@ -2,42 +2,49 @@ var $ = require("jquery");
 var md5 = require("md5");
 var moment = require("moment");
 
+async function exportMatch() {
+	try {
+		$("#processMatch-download").hide();
+		$("#processMatch-view").hide();
+		$("#view_data_currentlyViewing_short").html("Not Viewing Anything");
+		console.div("[exportMatch] Checking Fields");
+		if (dat.length !== 36) {
+			console.div("[PUBG_API] [exportMatch] Invalid MatchID");
+		} else {
+			var result = await require("./processMatch.js")($("#processMatch-id").val());
+			if (result === true) {
+				console.div("[PUBG_API] [exportMatch] An Error Occured. You should check the console.");
+				return;
+			} else {
+				$("#processMatch-download").show();
+				$("#processMatch-view").show();
+
+				var dsFN_o = { "antiDupePrefix": randomString(16), "matchTime": moment(result.attributes.createdAt).unix(), "hash": md5("id-"+result.id+" t-"+moment(result.attributes.createdAt).unix()), };
+				var downloadString_fileName = `match-${dsFN_o.antiDupePrefix}-${dsFN_o.matchTime}.json`;
+				console.div("[exportMatch] Match Ready to Download or View");
+				$("#processMatch-view").click(()=>{
+					$("#view_data_currentlyViewing_short").html("Ready to View a Match");
+				})
+				require("./MatchToHTML.js")(result)
+				$("#processMatch-download").click(() => {
+					downloadString(downloadString_fileName,JSON.stringify(result,null,"\t"));
+					console.div("[exportMatch] Downloaded Match "+result.id);
+				})
+			}
+		}
+	} catch(e) {
+		console.div("[PUBG_API] [exportMatch] An Error Occurred, Check Console");
+		console.error(e);
+	}
+}
+
 module.exports = function() {
 	if (!PUBG_connectionWorking) return;
 
-	$("#processMatch-submit").click(async () => {
-		try {
-			$("#processMatch-download").hide();
-			$("#processMatch-view").hide();
-			$("#view_data_currentlyViewing_short").html("Not Viewing Anything");
-			console.div("[exportMatch] Checking Fields");
-			if ($("#processMatch-id").val().length !== 36) {
-				console.div("[PUBG_API] [exportMatch] Invalid MatchID");
-			} else {
-				var result = await require("./processMatch.js")($("#processMatch-id").val());
-				if (result === true) {
-					console.div("[PUBG_API] [exportMatch] An Error Occured. You should check the console.");
-					return;
-				} else {
-					$("#processMatch-download").show();
-					$("#processMatch-view").show();
-
-					var dsFN_o = { "antiDupePrefix": randomString(16), "matchTime": moment(result.attributes.createdAt).unix(), "hash": md5("id-"+result.id+" t-"+moment(result.attributes.createdAt).unix()), };
-					var downloadString_fileName = `match-${dsFN_o.antiDupePrefix}-${dsFN_o.matchTime}.json`;
-					console.div("[exportMatch] Match Ready to Download or View");
-					$("#processMatch-view").click(()=>{
-						$("#view_data_currentlyViewing_short").html("Ready to View a Match");
-					})
-					require("./MatchToHTML.js")(result)
-					$("#processMatch-download").click(() => {
-						downloadString(downloadString_fileName,JSON.stringify(result,null,"\t"));
-						console.div("[exportMatch] Downloaded Match "+result.id);
-					})
-				}
-			}
-		} catch(e) {
-			console.div("[PUBG_API] [exportMatch] An Error Occurred, Check Console");
-			console.error(e);
-		}
+	$("#processMatch-submit").click(async (dat) => {
+		exportMatch(dat.val());
+	})
+	$("#viewUser_selectMatch").click(async () => {
+		exportMatch($(this).prop("name"));
 	})
 }();
