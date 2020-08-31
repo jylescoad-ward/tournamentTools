7328ba5bc79a2b6c020910b1479a8b51311ebe92
Jyles Coad-Ward <jyles@dariox.club>
2020-08-31 12:26:39 +0800

    Update exportMatch.js
    uses custom loader module

--- public/src/processing/exportMatch.js
+++ public/src/processing/exportMatch.js
@@ -4,16 +4,23 @@ var moment = require("moment");
 
 async function exportMatch() {
 	try {
+		custLoader.show()
 		$("#processMatch-download").hide();
 		$("#processMatch-view").hide();
 		$("#view_data_currentlyViewing_short").html("Not Viewing Anything");
+		custLoader.context("Checking Fields");
 		console.div("[exportMatch] Checking Fields");
 		if (dat.length !== 36) {
 			console.div("[PUBG_API] [exportMatch] Invalid MatchID");
+			custLoader.context("Invalid MatchID");
+			custLoader.hide()
 		} else {
+			custLoader.context("Fields Valid");
+			custLoader.context("Processing Match");
 			var result = await require("./processMatch.js")($("#processMatch-id").val());
 			if (result === true) {
 				console.div("[PUBG_API] [exportMatch] An Error Occured. You should check the console.");
+				custLoader.hide()
 				return;
 			} else {
 				$("#processMatch-download").show();
@@ -22,6 +29,7 @@ async function exportMatch() {
 				var dsFN_o = { "antiDupePrefix": randomString(16), "matchTime": moment(result.attributes.createdAt).unix(), "hash": md5("id-"+result.id+" t-"+moment(result.attributes.createdAt).unix()), };
 				var downloadString_fileName = `match-${dsFN_o.antiDupePrefix}-${dsFN_o.matchTime}.json`;
 				console.div("[exportMatch] Match Ready to Download or View");
+				custLoader.context("Match Ready to View");
 				$("#processMatch-view").click(()=>{
 					$("#view_data_currentlyViewing_short").html("Ready to View a Match");
 				})
@@ -30,6 +38,7 @@ async function exportMatch() {
 					downloadString(downloadString_fileName,JSON.stringify(result,null,"\t"));
 					console.div("[exportMatch] Downloaded Match "+result.id);
 				})
+				custLoader.hide();
 			}
 		}
 	} catch(e) {
