a3b4a7569b6281c8f562a1c03c14359ef4e39dc9
Jyles Coad-Ward <jyles@dariox.club>
2020-08-31 14:45:46 +0800

    Added support for custom loader

--- public/src/processing/exportUser.js
+++ public/src/processing/exportUser.js
@@ -6,6 +6,8 @@ module.exports = function() {
 	if (!PUBG_connectionWorking) return;
 
 	$("#processUser-submit").click(async () => {
+		custLoader.show()
+		custLoader.context("Parsing Fields")
 		$("#processUser-download").hide();
 		$("#processUser-view").hide();
 		$("#view_data_currentlyViewing_short").html("Not Viewing Anything");
@@ -13,8 +15,10 @@ module.exports = function() {
 			var result = await require("./processUser.js")($("#processUser-id").val());
 			if (result === true) {
 				console.div("[PUBG_API] [exportMatch] An Error Occured.");
+				custLoader.hide()
 				return;
 			} else {
+				custLoader.context("Retrieved User from PUBG Servers")
 				$("#processUser-download").fadeIn("fast");
 				$("#processUser-view").fadeIn("fast");
 
@@ -22,6 +26,7 @@ module.exports = function() {
 
 				var downloadString_fileName = `user-${result[0].attributes.name}-${moment(new Date()).unix()}.json`;
 				console.div("[PUBG_API] [exportUser] User Ready to Download");
+				custLoader.context("Finished Processing User");
 				$("#processUser-view").click(()=>{
 					$("#view_data_currentlyViewing_short").html(`Ready to view ${result[0].attributes.name}`);
 				})
@@ -32,6 +37,7 @@ module.exports = function() {
 					downloadString(downloadString_fileName,protectedFile);
 					console.div("[PUBG_API] [exportUser] Downloaded User "+result[0].attributes.name);
 				})
+				custLoader.hide()
 			}
 		} catch(e) {
 			console.div("[PUBG_API] [exportUser] An Error Occurred, Check Console");
