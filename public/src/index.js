c03c18238359efbaf917e188ecc61a7ce5174958
Jyles Coad-Ward <jyles@dariox.club>
2020-09-01 18:44:10 +0800

    Update index.js
    minifiyed console.div into a single line global function
    
    fixed anti-debug

--- public/src/index.js
+++ public/src/index.js
@@ -1,21 +1,32 @@
 import _ from 'lodash';
 import $ from 'jquery';
+global.console.div = function(message) { var length = 6; var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; var retVal = ""; for (var i = 0, n = charset.length; i < length; ++i) { retVal += charset.charAt(Math.floor(Math.random() * n)); } console.log(message); custLoader.context(message.trim()); $(`<span id="out-${retVal}">${message}</span><br>`).hide().appendTo("#tourn-consoleout"); $(`#out-${retVal}`).fadeIn("fast"); delete(length,charset,retVal); };
 
-import "./misc.js";
-import "./keyCheck.js";
-import "./customConsole.js";
-import "./dropdown.js";
-import "./buildInfo.js"
-
+// Declear Global Variables
 global.$ = require("jquery")
 global.custLoader = require("./loader.js");
 
-if (require("./../webpack.config.js").mode === "production") {
-	setTimeout(function() {
-		debugger;
-	},50);
-}
-
+// Runs when page is loaded.
 $(document).ready(() => {
     console.div(`[System] Page Ready!`);
+})
+
+// Start loading required modules for torunamentTools to function
+import("./misc.js");
+import("./keyCheck.js");
+import("./dropdown.js");
+import("./buildInfo.js");
+//template
+//require("./.js");
+
+
+// If set to production start anti-debugger stuff
+$(document).ready(()=>{
+	setTimeout(()=>{
+		if (require("./../webpack.config.js").mode === "production") {
+			setTimeout(function() {
+				debugger;
+			},500);
+		}
+	},5000)
 })
