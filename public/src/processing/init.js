23a0d0aaa984d2d338ec9a287cecdd9468e59fe0
Jyles Coad-Ward <jyles@dariox.club>
2020-09-10 00:19:35 +0800

    support for proxyManager

--- public/src/processing/init.js
+++ public/src/processing/init.js
@@ -9,14 +9,20 @@
 
 /* === Init PUBG API === */
 import $ from "jquery";
-setTimeout(async () => {
-	if (getCookie("apiKey").length === 264) {
-		var PUBG = require("battlegrounds-revisited");
-		global.PUBG_api = new PUBG(getCookie("apiKey"));
-		if (getCookie("keyValid") === "true") {
-			console.log("[PUBG_API] [processing] Called User and Match Modules.");
-			require("./user.js");
-			require("./match.js");
+$(document).ready(()=>{
+	setTimeout(async () => {
+		if (getCookie("apiKey").length === 264) {
+			var PUBG = require("battlegrounds-revisited");
+			var settings = {
+				key: getCookie("apiKey"),
+				proxyAddress: getCookie("proxyAddress"),
+			};
+			global.PUBG_api = new PUBG(settings);
+			if (getCookie("keyValid") === "true") {
+				console.log("[PUBG_API] [processing] Called User and Match Modules.");
+				require("./user.js");
+				require("./match.js");
+			}
 		}
-	}
-},500)
+	},500)
+})
