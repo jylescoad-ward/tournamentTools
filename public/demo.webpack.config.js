135c1e8b510cab63a4f330e17bd1e5a3d0addd8b
Jyles Coad-Ward <jyles@dariox.club>
2020-09-09 14:45:17 +0800

    created demo config

---
+++ public/demo.webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    target: "web",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                loader: 'css-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 16384,
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.js', '.css']
+    },
+    performance: {
+        hints: "warning",
+        maxAssetSize: 524288,
+        maxEntrypointSize: 3145728,
+        assetFilter: function(assetFileName){
+            return assetFileName.endsWith('.css') || assetFileName.endsWith(".js");
+        }
+    },
+    output: {
+        filename: 'demo.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+};
