9668a004515d73f271f86cd4dab6181751385999
Jyles Coad-Ward <jyles@dariox.club>
2020-09-10 02:51:33 +0800

    Update init.js

--- public/src/processing/init.js
+++ public/src/processing/init.js
@@ -13,11 +13,10 @@ $(document).ready(()=>{
 	setTimeout(async () => {
 		if (getCookie("apiKey").length === 264) {
 			var PUBG = require("battlegrounds-revisited");
-			var settings = {
+			global.PUBG_api = new PUBG({
 				key: getCookie("apiKey"),
 				proxyAddress: getCookie("proxyAddress"),
-			};
-			global.PUBG_api = new PUBG(settings);
+			});
 			if (getCookie("keyValid") === "true") {
 				console.log("[PUBG_API] [processing] Called User and Match Modules.");
 				require("./user.js");
